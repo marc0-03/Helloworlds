d7086983f10923a696ec8d6b54290f16d531a507
marc0-03 <marcus.nylund@elev.ga.ntig.se>
2020-10-13 09:50:42 +0200

    Initial commit

---
+++ src/Helloworld.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+public class Helloworld {
+        public static void main(String[] args) {
+            int jog = 5;
+            int jag = 7;
+            int jeg = jag*jog;
+            System.out.println(jog + " * " + jag + " = " + jeg);
+            JOptionPane.showMessageDialog(null, "helloworld");
+        }
+    }
