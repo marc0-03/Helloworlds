88baa5fc5df5c928250d83fda13dfeb6dabfe847
marc0-03 <marcus.nylund@elev.ga.ntig.se>
2020-10-16 11:19:23 +0200

    yup ster a ster yuper ster

---
+++ src/While52.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class While52 {
+    public static void main(String[] args) {
+        String s = JOptionPane.showInputDialog("n");
+     double summa = 0;
+     double k = 1;
+     double n = Integer.parseInt(s);
+     while (k<=n)
+    }
+}
