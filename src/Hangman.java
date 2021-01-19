724a7f7608dbe438659abbcd96c7a567bd4616d6
marc0-03 <marcus.nylund@elev.ga.ntig.se>
2021-01-19 11:05:16 +0100

    Har gjort delen där du kan gissa på ordet och så att det tar dig olika väger om du har rätt eller fel, det enda som är kvar är att kunna göra om "_" till användares gissning (Kolla rad 28) och att fixa så att varje gång du får fel sparar bokstaven sig och att göra den "Hängda gubben"

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,14 +1,14 @@
-import yeee.While;
-
 import javax.swing.*;
 import java.util.Random;
 
 public class Hangman {
     public static void main(String[] args) {
         String word = pickRandomword();
-        String Wrongword = "";
+        String  Ourword;
         int k = 0;
         int d = 0;
+        int i;
+        char ourquess;
         String Visibleword = "";
 
         while (d<word.length()){     //vill kunna få längden på det vallda ordet
@@ -19,11 +19,20 @@ public class Hangman {
             }
             d++;
         }
-        JOptionPane.showMessageDialog(null, Visibleword + " " + word); //just checkin
         while (k<10 || !Visibleword.contains("_")){
-            k++;
-            System.out.println(Visibleword + "the word is" + word); //here we should print the hangman the quessed letters, the Visibleword and it should ask the user for a letter im not sure if i should use a JOptionpane or the console for this
+            Ourword = JOptionPane.showInputDialog(null, Visibleword + " " + word); //here we should print the hangman the quessed letters, the Visibleword and it should ask the user for a letter im not sure if i should use a JOptionpane or the console for this
+            ourquess = Ourword.charAt(0);
+            if (word.contains(Ourword)) {
+                for (i=0; i<word.length(); i++) {
+                    if (word.charAt(i) == ourquess) {
+                        Visibleword.charAt(i*2) = ourquess; //Question, how do i change a character at a certain point in a string
+                    }
+                }
+            } else {
+                k++;
+            }
         } //kommer utanför denna när när du har slut på gisnignar eller när du är klar
+        System.out.println(Visibleword + " the word is " + word);
     }
 
     private static String pickRandomword() {
