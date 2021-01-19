1eb9062c9a9e5a456c067c7ef35cb2d3657bc49e
marc0-03 <marcus.nylund@elev.ga.ntig.se>
2021-01-19 10:21:56 +0100

    Har fixat så att proggramet kan see vilka bokstäver är "empty" och kan nu förvandla ordet till en rad av "_ _ _ _   _ _ _   _ _ _ _ _ "

--- src/Hangman.java
+++ src/Hangman.java
@@ -9,25 +9,25 @@ public class Hangman {
         String Wrongword = "";
         int k = 0;
         int d = 0;
-        String Visibleword = "_";
+        String Visibleword = "";
 
         while (d<word.length()){     //vill kunna få längden på det vallda ordet
-            if (word.charAt(d) != " ") {                 //Vill kunna kolla om karaktären i positionen d
-                Visibleword = Visibleword + " _";        // är " " och om det är det ha ett mellanrum där
+            if (word.charAt(d) != ' ') {                 //Vill kunna kolla om karaktären i positionen d
+                Visibleword = Visibleword + "_ ";        // är " " och om det är det ha ett mellanrum där
             } else {
                 Visibleword = Visibleword + "  ";
             }
             d++;
         }
-        JOptionPane.showMessageDialog(null, Visibleword + word); //just checkin
+        JOptionPane.showMessageDialog(null, Visibleword + " " + word); //just checkin
         while (k<10 || !Visibleword.contains("_")){
             k++;
-            System.out.println(); //here we should print the hangman the quessed letters, the Visibleword and it should ask the user for a letter im not sure if i should use a JOptionpane or the console for this
+            System.out.println(Visibleword + "the word is" + word); //here we should print the hangman the quessed letters, the Visibleword and it should ask the user for a letter im not sure if i should use a JOptionpane or the console for this
         } //kommer utanför denna när när du har slut på gisnignar eller när du är klar
     }
 
     private static String pickRandomword() {
-        String[] words = {"ett", "tvås", "Trete", "Jack O Lantern"};
+        String[] words = {"Jack O Lantern", "Jack O Lantern", "Jack O Lantern", "Jack O Lantern"};
         Random R = new Random();
         int randomNumber = R.nextInt(words.length);
         return words[randomNumber];
