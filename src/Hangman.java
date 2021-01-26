664efa76c630c7fca6d163f37955f2ac6bb7aa8d
marc0-03 <marcus.nylund@elev.ga.ntig.se>
2021-01-26 11:13:47 +0100

    felbokstäver lista, har en lista på vart gubben kommer att vara. och har en sak som kollar om du har gissat en bokstav du har redan gissat och har fått fel på, måste fixa så att det inte finns någon skillnad på stor och små bokstav

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.Random;
 
 public class Hangman {
@@ -9,6 +10,8 @@ public class Hangman {
         int d = 0;
         char ourquess;
         String Visibleword = "";
+        String[] gubben = {"Frame1","Frame2","Frame3","Frame4","Frame5","Frame6","Frame7","Frame8","Frame9","Frame10"};
+        String felgisning = "";
 
         while (d<word.length()){     //vill kunna få längden på det vallda ordet
             if (word.charAt(d) != ' ') {                 //Vill kunna kolla om karaktären i positionen d
@@ -19,16 +22,21 @@ public class Hangman {
             d++;
         }
         while (k<10 && Visibleword.contains("_")){
-            Ourword = JOptionPane.showInputDialog(null, Visibleword + " " + word); //here we should print the hangman the quessed letters, the Visibleword and it should ask the user for a letter im not sure if i should use a JOptionpane or the console for this
+            Ourword = JOptionPane.showInputDialog(null, gubben[k] + "\nDina Bokstäver du har gissat: " + felgisning + "\n" + Visibleword + " " + word); //here we should print the hangman the quessed letters, the Visibleword and it should ask the user for a letter im not sure if i should use a JOptionpane or the console for this
             ourquess = Ourword.charAt(0);
-            if (word.contains(Ourword)) {
-                for (int i=0; i<word.length(); i++) {
-                    if (word.charAt(i) == ourquess) {
-                        Visibleword = Visibleword.substring(0,i*2)+word.charAt(i)+Visibleword.substring((i*2)+1);
+            if (! felgisning.contains(Ourword)) {
+                if (word.contains(Ourword)) {
+                    for (int i = 0; i < word.length(); i++) {
+                        if (word.charAt(i) == ourquess) {
+                            Visibleword = Visibleword.substring(0, i * 2) + word.charAt(i) + Visibleword.substring((i * 2) + 1);
+                        }
                     }
+                } else {
+                    k++; //fick ett fel, borde också lägga till den gissade bokstaven till en lista
+                    felgisning = felgisning + ourquess + ",";
                 }
             } else {
-                k++; //fick ett fel, borde också lägga till den gissade bokstaven till en lista
+                JOptionPane.showMessageDialog(null, "Du har redan gissat " + Ourword);
             }
         } //kommer utanför denna när när du har slut på gisnignar eller när du är klar
         JOptionPane.showMessageDialog(null, "yooo");
