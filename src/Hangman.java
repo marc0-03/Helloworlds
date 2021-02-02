06eb573ceab99e55061353ccf9363789f427a0ba
marc0-03 <marcus.nylund@elev.ga.ntig.se>
2021-02-02 11:10:43 +0100

    Har fixat med att du kan välja antal försök och fixade så att om du gissar "" kommer inte programmet krasha

--- src/Hangman.java
+++ src/Hangman.java
@@ -9,6 +9,8 @@ public class Hangman {
         int d = 0;
         int Tries = 0;
         char ourquess;
+        int försök = 0;
+        String Check;
         String Visibleword = "";
         String felgisning = "";
         String[] gubben = {" ------\n" +
@@ -108,14 +110,18 @@ public class Hangman {
             }
             d++;
         }
-        while (k<10 && Visibleword.contains("_")){
-            Ourword = JOptionPane.showInputDialog(null, gubben[k] + "\nBokstäver du har gissat: " + felgisning + "\n" + Visibleword); //here we should print the hangman the quessed letters, the Visibleword and it should ask the user for a letter im not sure if i should use a JOptionpane or the console for this
+        försök = Integer.parseInt(JOptionPane.showInputDialog(null, "Hur många försök vill du ha\nRekomenderat mellan 8 och 10"));
+        while (k<försök && Visibleword.contains("_")){
+            Ourword = "";
+            while (Ourword.equals("")) {
+                    Ourword = JOptionPane.showInputDialog(null, gubben[(k * 10) / försök] + "\nBokstäver du har gissat: " + felgisning + "\nfel: " + k + "/" + försök + "\n" + Visibleword); //here we should print the hangman the quessed letters, the Visibleword and it should ask the user for a letter im not sure if i should use a JOptionpane or the console for this
+            }
             Ourword = Ourword.toUpperCase();
             ourquess = Ourword.charAt(0);
-            if (! felgisning.contains(Ourword)) {
+            if (! felgisning.contains(ourquess + "")) {
                 felgisning = felgisning + ourquess + ",";
                 Tries++;
-                if (word.contains(Ourword)) {
+                if (word.contains(ourquess+"")) {
                     for (int i = 0; i < word.length(); i++) {
                         if (word.charAt(i) == ourquess) {
                             Visibleword = Visibleword.substring(0, i * 2) + word.charAt(i) + Visibleword.substring((i * 2) + 1);
@@ -125,18 +131,18 @@ public class Hangman {
                     k++; //fick ett fel.
                 }
             } else {
-                JOptionPane.showMessageDialog(null, "Du har redan gissat " + Ourword);
+                JOptionPane.showMessageDialog(null, "Du har redan gissat " + ourquess);
             }
         } //kommer utanför denna när när du har slut på gisnignar eller när du är klar
-        if (k<10) {
-            JOptionPane.showMessageDialog(null,  gubben[k] + "\n\nDu vann! \nOrdet var " + word + "\nDet tog " + Tries + " försök att få ordet");
+        if (k<försök) {
+            JOptionPane.showMessageDialog(null,  gubben[(k*10)/försök] + "\n\nDu vann! \nOrdet var " + word + "\nDet tog " + Tries + " försök att få ordet");
         } else { //om du van ser du medelandet över och om du förlorade ser du det som är under
-            JOptionPane.showMessageDialog(null,  gubben[k] + "\n\nFan han dog! \nDet här är ditt fel, ordet var " + word + " det borde inte varit så svårt\nDet tog dig " + Tries + " försök Men endå kunde du inte rädda han");
+            JOptionPane.showMessageDialog(null,  gubben[(k*10)/försök] + "\n\nFan han dog! \nDet här är ditt fel, ordet var " + word + " det borde inte varit så svårt\nDet tog dig " + Tries + " försök Men endå kunde du inte rädda han");
         }
     }
 
     private static String pickRandomword() {
-        String[] words = {"JACK O LANTERN", "DANDARA", "UNDERTALE", "JACK THE RIPPER", "NEW PHONE WHO DIS", "PIE", "DEAD MEN TELL NO TALES", "MONKEY VS LIZARD"};
+        String[] words = {"JACK O LANTERN", "DANDARA", "UNDERTALE", "JACK THE RIPPER", "NEW PHONE WHO DIS", "PIE", "DEAD MEN TELL NO TALES", "MONKEY VS LIZARD", "yoooooo", ""};
         Random R = new Random();
         int randomNumber = R.nextInt(words.length);
         return words[randomNumber];
