71d7a151a2cf4f88916ac75d14f3b723b6d7c0c3
marc0-03 <marcus.nylund@elev.ga.ntig.se>
2021-02-26 11:19:03 +0100

    La in magnus testkod och började jobba på Komplex.java
    Jag tror jag han fixa så att man kan säga vad det komplexa talets vanliga sifra och det "I" ska vara

---
+++ src/Komplex.java
@@ -0,0 +1,14 @@
+public class Komplex {
+    public double vanligt;
+    public double Negativt;
+
+    public Komplex(double v, double I) {
+        this.vanligt = v;
+        this.Negativt = I;
+    }
+
+    public Komplex void add(Komplex b) { //vet inte hur jag ska göra det här
+    }
+
+    }
+
